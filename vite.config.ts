5f0c6ebfca206198900ec0372fd667cb3a591657
personal-22 <personal-22@github.com>
2026-05-06 18:41:04 +0700

    Final Fix: Clean dependencies and standard Vite config for Vercel

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,3 @@
-import crypto from 'node:crypto'
-
-// Polyfill for crypto.hash which is missing in Node.js < 21.7.0
-if (!crypto.hash) {
-  // @ts-ignore
-  crypto.hash = (algorithm, data, outputEncoding = 'hex') => {
-    return crypto.createHash(algorithm).update(data).digest(outputEncoding);
-  };
-}
-
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { tanstackStart } from '@tanstack/react-start/plugin/vite'
